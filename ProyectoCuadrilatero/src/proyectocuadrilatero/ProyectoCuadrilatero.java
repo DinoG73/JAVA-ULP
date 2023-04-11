3cec8e8feacc1176953098f2b4fa9ef524c75b9e
DinoG73 <53279025+DinoG73@users.noreply.github.com>
2023-04-11 10:11:48 -0300

    Comments deleted

--- ProyectoCuadrilatero/src/proyectocuadrilatero/ProyectoCuadrilatero.java
+++ ProyectoCuadrilatero/src/proyectocuadrilatero/ProyectoCuadrilatero.java
@@ -1,20 +1,12 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
+
+ 
 package proyectocuadrilatero;
 
-/**
- *
- * @author bruno
- */
+
 public class ProyectoCuadrilatero {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic
+        
         Cuadrilatero cuad1 = new Cuadrilatero(76, 52);
         
         System.out.println(cuad1.calcularArea()); 
