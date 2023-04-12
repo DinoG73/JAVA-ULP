ed7476423347d7fae05a9d7b355e6badd6da5cc8
DinoG73 <53279025+DinoG73@users.noreply.github.com>
2023-04-12 16:24:03 -0300

    ProyectoFecha

--- ProyectoFecha/src/Fecha.java
+++ ProyectoFecha/src/Fecha.java
@@ -1,4 +1,58 @@
 
-public class Fecha {
 
+public class Fecha {
+	private  int dia;
+	private  int mes;
+	private  int ano;
+	
+	
+	public Fecha(int dia, int mes, int ano) {
+		this.dia = dia;
+		this.mes = mes;
+		this.ano = ano;
+	}
+
+
+	public int getDia() {
+		return dia;
+	}
+
+
+	public void setDia(int dia) {
+		this.dia = dia;
+	}
+
+
+	public int getMes() {
+		return mes;
+	}
+
+
+	public void setMes(int mes) {
+		this.mes = mes;
+	}
+
+
+	public int getAno() {
+		return ano;
+	}
+
+
+	public void setAno(int ano) {
+		this.ano = ano;
+	}
+	
+	public boolean esBisiesto() {
+		return this.ano % 4 == 0;
+	}
+	
+	/*calcularAños(): Este método recibirá un objeto del tipo Fecha y retornará un entero que 
+será la cantidad de años transcurridos entre esta Fecha y la que recibe por parámetro.*/
+	public int calcuarAnios(Fecha fecha1) {
+		int aniosTranscurridos = fecha1.ano -this.ano;
+		if(fecha1.mes < this.mes || (fecha1.mes == this.mes && fecha1.dia < this.dia)) {
+			aniosTranscurridos--;
+		}
+		return aniosTranscurridos;
+	}
 }
--- ProyectoFecha/src/MainFecha.java
+++ ProyectoFecha/src/MainFecha.java
@@ -2,8 +2,14 @@
 public class MainFecha {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Fecha date1 = new Fecha(27, 6, 2006);
+		Fecha date2 = new Fecha(27, 6, 2008);
+		
+		System.out.println(date1.esBisiesto());
+		System.out.println(date2.esBisiesto());
+		System.out.println(date1.calcuarAnios(date2));
+		
+		
 	}
 
 }
